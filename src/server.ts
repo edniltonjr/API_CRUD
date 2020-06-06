7135b54888c700542df062ed2cd62c0af7b71e90
Ednilton Junior <jrjunior_cg@live.com>
2020-06-06 00:35:19 -0300

    Att

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use('/crms', crmsRoutes);
 app.use('/user', userRoutes);
 app.use('/receipt', receiptRoutes);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server Started on Port 3000!');
 });
 
